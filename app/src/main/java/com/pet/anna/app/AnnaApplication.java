969ab59e725948325e8d2aca1406cc5c2aa6ea34
EugeneKhramtsov <yevheniikhramtsov@gmail.com>
2020-08-04 16:51:47 +0300

    Initial commit

---
+++ app/src/main/java/com/pet/anna/app/AnnaApplication.java
@@ -0,0 +1,12 @@
+package com.pet.anna.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnnaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnnaApplication.class, args);
+	}
+}
---
+++ app/src/test/java/com/pet/anna/app/AnnaApplicationTests.java
@@ -0,0 +1,12 @@
+package com.pet.anna.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnnaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+}
